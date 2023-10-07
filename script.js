7a115447e8f81ac389d390607d62b608afd97809
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-07 14:01:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Estructura HTML del proyecto. 
+// Variables de JS necesarias. 
+// Funciones esenciales del proceso a simular.
+// Objetos de JS.
+// Arrays.
+// Métodos de búsqueda y filtrado sobre el Array.
+
+const array = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!@#$%^&*()-_+=[]{};:,.<>?"]
+let contraseñaGenerada = ""
+
+function 
