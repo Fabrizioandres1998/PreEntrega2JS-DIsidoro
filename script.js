3985195ce29a821c6f12107a1a0c93c0e1df3822
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-09 00:40:54 -0300

    feat(js): codigo corriendo correctamente

--- script.js
+++ script.js
@@ -5,7 +5,61 @@
 // Arrays.
 // Métodos de búsqueda y filtrado sobre el Array.
 
-const array = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!@#$%^&*()-_+=[]{};:,.<>?"]
+const array = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789"]
+let longitudContraseña = parseInt(prompt("Ingresa el largo de la contraseña que deseas"))
 let contraseñaGenerada = ""
+let paridad = ""
+let contraseña = ""
+let mayusculas = ""
+let minusculas = ""
+let numeros = ""
 
-function 
+function generarContraseña() {
+    for (let i = 0; i < longitudContraseña; i++) {
+        const elementoAleatorio = Math.floor(Math.random() * array.length)
+        const indexAleatorio = Math.floor(Math.random() * array[elementoAleatorio].length)
+
+        let caracteres = array[elementoAleatorio].charAt(indexAleatorio)
+        contraseñaGenerada += caracteres
+    }
+
+    if (contraseñaGenerada.length % 2 === 0) {
+        paridad = "Par"
+    } else {
+        paridad = "Impar"
+    }
+
+    const conntieneMayusculas = contraseñaGenerada.split("").some(caracter => array[0].includes(caracter))
+    const conntieneMinusculas = contraseñaGenerada.split("").some(caracter => array[1].includes(caracter))
+    const conntieneNumeros = contraseñaGenerada.split("").some(caracter => array[2].includes(caracter))
+
+    if (conntieneMayusculas) {
+        mayusculas = "Si"
+    } else {
+        mayusculas = "No"
+    }
+    if (conntieneMinusculas) {
+        minusculas = "Si"
+    } else {
+        minusculas = "No"
+    }
+    if (conntieneNumeros) {
+        numeros = "Si"
+    } else {
+        numeros = "No"
+    }
+
+    contraseña = {
+        "Contraseña": contraseñaGenerada,
+        "Longitud": longitudContraseña,
+        "Paridad de la longitud": paridad,
+        "Incluye mayusculas": mayusculas,
+        "Incluye minusculas": minusculas,
+        "Incluye numeros": numeros
+    }
+
+}
+
+generarContraseña()
+alert("Tu contraseña es: " + contraseñaGenerada)
+console.log(contraseña)
